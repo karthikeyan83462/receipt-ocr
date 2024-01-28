ceb21370dcf60b2ad40d68497c4187750afd684c
Karthikeyan <120308685+karthikeyan83462@users.noreply.github.com>
2024-01-28 11:43:16 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,4 @@
 import re
-import os
-import json
-import cv2
-import pytesseract
-import numpy as np
-from pytesseract import Output
 
 def clean_special_characters(name, excluded_words=None):
     if excluded_words is None:
@@ -12,45 +6,37 @@ def clean_special_characters(name, excluded_words=None):
     return re.sub(r'[^a-zA-Z\s]', '', name).replace('\n', ' ').replace(''.join(excluded_words), '').strip()
 
 def clean_n_s(name):
-    name = name.replace("\n","")
-    name = name.replace(" ","")
-    return name
+    return name.replace("\n","").replace(" ","")
 
-def extract_items_and_costs(text,keywords,remove_list):
+def extract_items_and_costs(text, keywords, remove_list):
     item_cost = []
     items = []
     for line in text.splitlines():
-        if re.search(r"[0-9]*\.[0-9]|[0-9]*\,[0-9]", line):
-            regex_line = line
-            if not any(exclude in regex_line.lower() for exclude in keywords):
-                new_item = regex_line
-                for subToRemove in remove_list:
-                    new_item = new_item.replace(subToRemove, "").replace(subToRemove.upper(), "")
-                new_item_list = ' '.join(ele for ele in new_item.split() if len(ele) != 2)
-                cleaned_item = clean_special_characters(new_item_list)
-                items.append(cleaned_item)
-                cost = re.findall('\d*\.?\d+|\d*\,?\d+|', line.replace(",", "."))
-                item_cost.extend(possibleCost for possibleCost in cost if "." in possibleCost)
+        if any(re.search(r"[0-9]*[.,]?[0-9]+", line) for _ in keywords) and not any(exclude in line.lower() for exclude in keywords):
+            new_item = line
+            for subToRemove in remove_list:
+                new_item = new_item.replace(subToRemove, "").replace(subToRemove.upper(), "")
+            cleaned_item = clean_special_characters(new_item)
+            items.append(cleaned_item)
+            cost = re.findall('\d*\.?\d+', line.replace(",", "."))
+            item_cost.extend(cost)
     return items, item_cost
 
-def extract_values(text, keywords,remove_list):
+def extract_values(text, keywords, remove_list):
     val_cost = []
     vals = []
     for line in text.splitlines():
-        if re.search(r"[0-9]*\.[0-9]|[0-9]*\,[0-9]", line):
-            regex_line = line
-            if any(exclude in regex_line.lower() for exclude in keywords):
-                new_val = regex_line
-                for subToRemove in remove_list:
-                    new_val = new_val.replace(subToRemove, "").replace(subToRemove.upper(), "")
-                new_val_list = ' '.join(ele for ele in new_val.split() if len(ele) != 2)
-                cleaned_val = clean_special_characters(new_val_list)
-                vals.append(cleaned_val)
-                cost = re.findall('\d*\.?\d+|\d*\,?\d+|', line.replace(",", "."))
-                val_cost.extend(possibleCost for possibleCost in cost if "." in possibleCost)
+        if any(re.search(r"[0-9]*[.,]?[0-9]+", line) for _ in keywords) and any(exclude in line.lower() for exclude in keywords):
+            new_val = line
+            for subToRemove in remove_list:
+                new_val = new_val.replace(subToRemove, "").replace(subToRemove.upper(), "")
+            cleaned_val = clean_special_characters(new_val)
+            vals.append(cleaned_val)
+            cost = re.findall('\d*\.?\d+', line.replace(",", "."))
+            val_cost.extend(cost)
     return vals, val_cost
 
-def extract_merchant_info(text,excluded_words):
+def extract_merchant_info(text):
     merchant_name_match = re.search(r'^\s*(.+?)(?:\d|^\s*$)', text, re.MULTILINE | re.DOTALL)
     phone_number_match = re.search(r'\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}', text)
     email_match = re.search(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', text, re.IGNORECASE)
@@ -66,18 +52,13 @@ def extract_merchant_info(text,excluded_words):
     ticket_number_match = re.search(r'\b(?:Ticket|TICKET|Ticket #)\s*[:#]?\s*([a-zA-Z0-9]+\s*[a-zA-Z0-9]*)', text, re.IGNORECASE)
     ref_number_match = re.search(r'\b(?:REF|ref)\s*[:#]?\s*([a-zA-Z0-9]+\s*[a-zA-Z0-9]*)', text, re.IGNORECASE)
     station_number_match = re.search(r'\b(?:Station|STATION)\s*[:#]?\s*([a-zA-Z0-9]+\s*[a-zA-Z0-9]*)', text, re.IGNORECASE)
-
-    #order_number_match = re.search(r'\b(?:Order|ORDER|Check|CHECK|Chk|CHK|Order #)\s*[:#]?\s*([a-zA-Z0-9]+\s*[a-zA-Z0-9]*)\s', text, re.IGNORECASE)
     guest_match = re.search(r'\b(?:Guests|GUESTS|Persons|PERSONS|Guest|GUEST)\s*[:#]?\s*([a-zA-Z0-9]+\s*[a-zA-Z0-9]*)\s', text, re.IGNORECASE)
     dine_in_match = re.search(r'\bDine In\b', text, re.IGNORECASE)
     
-    
-    
     merchant_name = merchant_name_match.group(1).strip() if merchant_name_match else "Null"
-    cleaned_merchant_name = clean_special_characters(merchant_name, excluded_words)
-    email_match = clean_n_s(email_match.group(0).strip() if email_match else "Null")
-    website = website_match.group(1).strip() if website_match else "Null"
-    website = clean_n_s(f"www.{website}.com" if website != "Null" else "Null")
+    cleaned_merchant_name = clean_special_characters(merchant_name)
+    email_match = clean_n_s(email_match.group(0).strip()) if email_match else "Null"
+    website = clean_n_s(f"www.{website_match.group(1).strip()}.com") if website_match else "Null"
     server_name = server_name_match.group(1).strip() if server_name_match else "Null"
     user_name = user_name_match.group(1).strip() if user_name_match else "Null"
     sales_rep_name = sales_rep_name_match.group(1).strip() if sales_rep_name_match else "Null"
@@ -87,26 +68,13 @@ def extract_merchant_info(text,excluded_words):
     merchant_address = merchant_address.group(1).replace('\n', ' ').strip() if merchant_address else "Null"
     merchant_address = merchant_address[:60]
 
-    for word in excluded_words:
-        merchant_address = re.sub(rf'\b{word}\b', '', merchant_address, flags=re.IGNORECASE).strip()
-
     date_time = date_time_match.group(0) if date_time_match else "Null"
     table_number = table_number_match.group(1).strip() if table_number_match else "Null"
-    order = re.findall(r'\d+', order_number_match.group(1) if order_number_match else "Null")
-    order_number = order[0] if order else "Null"
-    
-    ref = re.findall(r'\d+', ref_number_match.group(1) if ref_number_match else "Null")
-    ref_number = ref[0] if ref else "Null"
-    
-    ticket = re.findall(r'\d+', ticket_number_match.group(1) if ticket_number_match else "Null")
-    ticket_number = ticket[0] if ticket else "Null"
-
-    station = re.findall(r'\d+', station_number_match.group(1) if station_number_match else "Null")
-    station_number = station[0] if station else "Null"
-    
-    guest = re.findall(r'\d+', guest_match.group(1) if guest_match else "Null")
-    guest_number = guest[0] if guest else "Null"
-    
+    order_number = re.findall(r'\d+', order_number_match.group(1) if order_number_match else "Null")[0] if order_number_match else "Null"
+    ref_number = re.findall(r'\d+', ref_number_match.group(1) if ref_number_match else "Null")[0] if ref_number_match else "Null"
+    ticket_number = re.findall(r'\d+', ticket_number_match.group(1) if ticket_number_match else "Null")[0] if ticket_number_match else "Null"
+    station_number = re.findall(r'\d+', station_number_match.group(1) if station_number_match else "Null")[0] if station_number_match else "Null"
+    guest_number = re.findall(r'\d+', guest_match.group(1) if guest_match else "Null")[0] if guest_match else "Null"
     dine_in = bool(dine_in_match)
 
     return {
